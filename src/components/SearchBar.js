18018bb0255e3992cf8effdded6a01909351c36a
Jason Toups <me@jasontoups.com>
2021-11-12 10:51:38 -0800

    debugging searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import reactLogo from "../style/icons/reactLogo.svg";
+// import reactLogo from "../style/icons/reactLogo.svg";
 
 class SearchBar extends React.Component {
 	state = { term: '' };
