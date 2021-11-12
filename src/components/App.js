263cd60f6a7a84764b269e4ff262f3e0218eda4a
Jason Toups <me@jasontoups.com>
2021-11-12 11:34:52 -0800

    completes functional refactor of App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
+const App = () => {
+  const [images, setImages] = useState([]);
 
-class App extends React.Component {
-  state = {
-    images: []
-  };
-  // TODO create a default search term to render images on page load. useEffect would help here.
-  onSearchSubmit = async (term) => {
+  useEffect(() => {
+		onSearchSubmit('react js');
+	}, []);
+
+  const onSearchSubmit = async (term) => {
     const response = await unsplash
       .get('/search/photos', {
         params: { query: term }
       })
-    this.setState({ images: response.data.results });
+      console.log(images)
+      setImages(response.data.results);
   }
 
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
-      </div>
-    );
-  }
+  return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
+    </div>
+  );
+
 }
 
 export default App;
